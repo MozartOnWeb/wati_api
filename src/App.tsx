51c508a3f326d1712e6ac296330c7a582f7edef8
Mandjou Dama <79732869+MozartOnWeb@users.noreply.github.com>
2023-07-17 14:27:13 +0000

    working on wati api

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import axios from "axios";
+
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const ACCESS_TOKEN =
+    "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJmNDU3ZmIyZi0wMGI4LTQ4MGQtOTFmZS04ZDZiMjczMTY2MDIiLCJ1bmlxdWVfbmFtZSI6Im1hbmRqb3VkYW1hQGdtYWlsLmNvbSIsIm5hbWVpZCI6Im1hbmRqb3VkYW1hQGdtYWlsLmNvbSIsImVtYWlsIjoibWFuZGpvdWRhbWFAZ21haWwuY29tIiwiYXV0aF90aW1lIjoiMDcvMTcvMjAyMyAxMzozMjo0NSIsImRiX25hbWUiOiJ3YXRpX2FwcF90cmlhbCIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvcm9sZSI6IlRSSUFMIiwiZXhwIjoxNjkwMjQzMjAwLCJpc3MiOiJDbGFyZV9BSSIsImF1ZCI6IkNsYXJlX0FJIn0.r8I0smfsSOipa3t26hsQbheFfJlZIbp18RzUkFSxxeo";
+
+  const handleSend = () => {
+    const options = {
+      method: "POST",
+      url: "https://app-server.wati.io/api/v1/sendTemplateMessage",
+      params: { whatsappNumber: "+22361344035" },
+      headers: {
+        "content-type": "text/json",
+        Authorization: ACCESS_TOKEN,
+      },
+      data: {
+        broadcast_name: "test_broadcast",
+        template_name: "children_toys",
+      },
+    };
+
+    axios
+      .request(options)
+      .then(function (response) {
+        console.log(response.data);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <button onClick={handleSend}>Send</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
