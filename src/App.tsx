b64e37e99f376482c8ca7ac53b9e7e59c5dac61e
Mandjou Dama <79732869+MozartOnWeb@users.noreply.github.com>
2023-07-18 20:19:09 +0000

    working on wati api

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,20 @@ import axios from "axios";
 import "./App.css";
 
 function App() {
-  const ACCESS_TOKEN =
-    "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJmNDU3ZmIyZi0wMGI4LTQ4MGQtOTFmZS04ZDZiMjczMTY2MDIiLCJ1bmlxdWVfbmFtZSI6Im1hbmRqb3VkYW1hQGdtYWlsLmNvbSIsIm5hbWVpZCI6Im1hbmRqb3VkYW1hQGdtYWlsLmNvbSIsImVtYWlsIjoibWFuZGpvdWRhbWFAZ21haWwuY29tIiwiYXV0aF90aW1lIjoiMDcvMTcvMjAyMyAxMzozMjo0NSIsImRiX25hbWUiOiJ3YXRpX2FwcF90cmlhbCIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvcm9sZSI6IlRSSUFMIiwiZXhwIjoxNjkwMjQzMjAwLCJpc3MiOiJDbGFyZV9BSSIsImF1ZCI6IkNsYXJlX0FJIn0.r8I0smfsSOipa3t26hsQbheFfJlZIbp18RzUkFSxxeo";
-
   const handleSend = () => {
     const options = {
       method: "POST",
-      url: "https://app-server.wati.io/api/v1/sendTemplateMessage",
-      params: { whatsappNumber: "+22361344035" },
+      url: "https://live-server-112768.wati.io/api/v1/sendTemplateMessage",
+      params: { whatsappNumber: "22361344035" },
       headers: {
         "content-type": "text/json",
-        Authorization: ACCESS_TOKEN,
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJhZjk5ZTliNi02MjcyLTQ5YjYtODNmMy0xZDUzZGI3OGQxYzMiLCJ1bmlxdWVfbmFtZSI6ImRhbWEubWFuZGpvdUBrYWJha29vLmFmcmljYSIsIm5hbWVpZCI6ImRhbWEubWFuZGpvdUBrYWJha29vLmFmcmljYSIsImVtYWlsIjoiZGFtYS5tYW5kam91QGthYmFrb28uYWZyaWNhIiwiYXV0aF90aW1lIjoiMDcvMTgvMjAyMyAxODo0ODoxOCIsImRiX25hbWUiOiIxMTI3NjgiLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3JvbGUiOiJERVZFTE9QRVIiLCJleHAiOjI1MzQwMjMwMDgwMCwiaXNzIjoiQ2xhcmVfQUkiLCJhdWQiOiJDbGFyZV9BSSJ9.ezqtX_OIjz9QYIEDHdwWCJjoF_eBHRiS7NZl8eNEs2E",
       },
       data: {
-        broadcast_name: "test_broadcast",
-        template_name: "children_toys",
+        parameters: [{ name: "name", value: "name" }],
+        broadcast_name: "wati_api",
+        template_name: "wati_api",
       },
     };
 
@@ -33,7 +32,7 @@ function App() {
 
   return (
     <>
-      <button onClick={handleSend}>Send</button>
+      <button onClick={() => handleSend()}>Send</button>
     </>
   );
 }
